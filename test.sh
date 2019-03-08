1b813076895a6008f62b09c6364b4c16143d8355
oopilot5 <jason.ljj.dev@gmail.com>
2019-03-08 21:10:09 +0800

    update

--- test.sh
+++ test.sh
@@ -5,50 +5,50 @@ mkdir build
 cd build
 
 ## 默认情况 
+echo "\n\n!!! default !!!"
 cmake ..
 make
-echo "!!! default !!!"
 readelf -S gcc64make |grep debug  
 ./gcc64make
 ## 默认是debug 执行了assert, 但是不加debug参数 readelf 读不出来debug 信息
 
 ## release
+echo "\n\n!!! release !!!"
 rm -rf *
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
-echo "!!! release !!!"
 readelf -S gcc64make |grep debug
 ./gcc64make
 
 ## debug
+echo "\n\n!!! debug !!!"
 rm -rf *
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
-echo "!!! debug !!!"
 readelf -S gcc64make |grep debug
 ./gcc64make
 
 ## 32位 默认
+echo "\n\n!!! 32 default !!!"
 rm -rf *
 cmake -DUSE_32BITS=1 ..
 make
-echo "!!! 32 default !!!"
 readelf -S gcc64make |grep debug
 ./gcc64make
 
 ## 32位 release
+echo "\n\n!!! 32 release !!!"
 rm -rf *
 cmake -DUSE_32BITS=1 -DCMAKE_BUILD_TYPE=Release ..
 make
-echo "!!! 32 release !!!"
 readelf -S gcc64make |grep debug
 ./gcc64make
 
 ## 32位 debug
+echo "\n\n!!! 32 debug !!!"
 rm -rf *
 cmake -DUSE_32BITS=1 -DCMAKE_BUILD_TYPE=Debug ..
 make
-echo "!!! 32 debug !!!"
 readelf -S gcc64make |grep debug
 ./gcc64make
 
--- testall.sh
+++ testall.sh
@@ -12,3 +12,6 @@ sh test.sh
 mv gcc64_make.cpp gcc64_make.c
 cd hello && mv hello.cpp hello.c && cd ../
 cd world && mv world.cpp world.c && cd ../
+
+#c++编译器 自动识别.cpp /usr/bin/c++ -> /etc/alternatives/c++ -> /usr/bin/g++
+#c编译器   自动识别.c   /usr/bin/cc  -> /etc/alternatives/cc  -> /usr/bin/gcc
